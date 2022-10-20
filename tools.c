e163092b8d7e7cdc303c0567ef02f40805f0ea34
Colg4teSmile <marcovenieri.flcl@gmail.com>
2022-10-20 23:22:52 +0200

    enhanced tool to dump memory

--- tools.c
+++ tools.c
@@ -4,18 +4,18 @@
 
 extern gap_buffer_t* gap_buf_ptr;
 
-void print_stats(void  /* TODO: gap_buffer_t* gap_buf_ptr */)
+void print_stats(gap_buffer_t* ptr)
 {
 	printf("***** STATS *****\n");
 	printf("Message:\n");
-	printf("%s", gap_buf_ptr->left);
-	//printf("%s\n", gap_buf_ptr->right + gap_buf_ptr->right_index);
-	printf("Left index: %d\n", gap_buf_ptr->left_index);
-	printf("Length of left message: %d\n", gap_buf_ptr->left_index);
-	printf("Right Index: %d\n", gap_buf_ptr->right_index);
-	printf("Length of right message: %d\n", (gap_buf_ptr->length >> 1) - gap_buf_ptr->right_index - 1);
-	printf("Total length of buffer: %d\n", gap_buf_ptr->length);
-	printf("Total length of the message: %d\n", gap_buf_ptr->left_index + (gap_buf_ptr->length >> 1) - gap_buf_ptr->right_index - 1);
+	printf("%s", ptr->left);
+	//printf("%s\n", ptr->right + ptr->right_index);
+	printf("Left index: %d\n", ptr->left_index);
+	printf("Length of left message: %d\n", ptr->left_index);
+	printf("Right Index: %d\n", ptr->right_index);
+	printf("Length of right message: %d\n", (ptr->length >> 1) - ptr->right_index - 1);
+	printf("Total length of buffer: %d\n", ptr->length);
+	printf("Total length of the message: %d\n", ptr->left_index + (ptr->length >> 1) - ptr->right_index - 1);
 	printf("*****************\n\n");
 }
 
@@ -25,12 +25,19 @@ void dump(char buf[], int length)
 	for(int i = 0; i < length; i++)
 	{
 		printf("%02x ",buf[i]);
-		if(i + 1 % 8 == 0)
+		if((i + 1) % 8 == 0)
 		{
 			printf("\n");
 		}
 	}
 	printf("\n");
+}
+
+void dump_gap_buffer(gap_buffer_t* ptr)
+{
+	print_stats(ptr);
+    dump(ptr->left, ptr->length >> 1);
+    dump(ptr->right, ptr->length >> 1);
 }
 
 
